4992e533d82267b23e7503d0c57745cabc4a7743
TukyBoy007 <tcoreboy@gmail.com>
2024-09-08 20:02:23 +0800

    tester

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import ScrollObserver from '@/utils/scroll-observer';
+
+function MainApp({Component, pageProps}: AppProps){
+  return(
+    <ScrollObserver>
+      <Component {...pageProps}/>
+    </ScrollObserver>
+  )
+}
+export default MainApp;
