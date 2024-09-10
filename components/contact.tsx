8509d34435f3ae8526a68dfc472a39c8c91d5c5a
TukyBoy007 <tcoreboy@gmail.com>
2024-09-10 16:25:04 +0800

    tester

--- components/contact.tsx
+++ components/contact.tsx
@@ -18,19 +18,25 @@ const ContactPage: React.FC<ContactProps> = ({ data }) => {
           {/* Adjust left side to be smaller */}
           <div className="md:flex-1 md:basis-1/3 p-8 text-center md:text-left space-y-5">
             <h1 className="text-2xl font-bold">Contact Us</h1>
-            <p className="tracking-wide">
-              <FontAwesomeIcon
-                icon={faLocationArrow}
-                className="text-purple-200"
-              />
+            <p className="tracking-wide flex gap-2">
+              <div className="w-[80px] h-[80px]">
+                <FontAwesomeIcon
+                  icon={faLocationArrow}
+                  className="text-purple-200"
+                />
+              </div>
               {loc}
             </p>
-            <p className="tracking-wide">
-              <FontAwesomeIcon icon={faPhone} className="text-purple-200" />{" "}
+            <p className="tracking-wide flex gap-2">
+              <div className="w-[20px] h-[20px]">
+                <FontAwesomeIcon icon={faPhone} className="text-purple-200" />{" "}
+              </div>
               {phone1}
             </p>
-            <p className="tracking-wide">
-              <FontAwesomeIcon icon={faPhone} className="text-purple-200" />{" "}
+            <p className="tracking-wide flex gap-2">
+              <div className="w-[20px] h-[20px]">
+                <FontAwesomeIcon icon={faPhone} className="text-purple-200" />{" "}
+              </div>
               {phone2}
             </p>
           </div>
