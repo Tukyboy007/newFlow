a7285300c4ce0530bbaa39ba9c2dd378b2a6a9b0
Bilguun <Bilguun@gmail.com>
2024-10-01 09:13:50 +0800

    update

--- components/lang.tsx
+++ components/lang.tsx
@@ -1,4 +1,3 @@
-// components/LanguageSwitcher.tsx
 import { FC, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import i18n from "i18next";
@@ -6,20 +5,17 @@ import Flag from "react-world-flags";
 
 const LanguageSwitcher: FC = () => {
   const router = useRouter();
-  const [language, setLanguage] = useState(router.locale || "en"); // Get initial locale from Next.js router
+  const [language, setLanguage] = useState(router.locale || "en");
 
   useEffect(() => {
-    // Sync state with current router locale if page is refreshed
     setLanguage(router.locale || "en");
-  }, [router.locale]); // Run this effect whenever the locale changes
+  }, [router.locale]);
 
   const changeLanguage = (newLanguage: string) => {
     i18n
       .changeLanguage(newLanguage)
       .then(() => {
-        // Update the language in the state
         setLanguage(newLanguage);
-        // Change locale in Next.js
         router.push(router.pathname, router.asPath, { locale: newLanguage });
       })
       .catch((error) => {
@@ -28,22 +24,13 @@ const LanguageSwitcher: FC = () => {
   };
 
   return (
-    <div className="">
-      {language === "en" ? (
-        <button
-          onClick={() => changeLanguage("en")}
-          className="flex items-center space-x-2 p-2 "
-        >
-          <Flag code="GB" alt="English" width={30} height={20} />
-        </button>
-      ) : (
-        <button
-          onClick={() => changeLanguage("mn")}
-          className="flex items-center space-x-2 p-2"
-        >
-          <Flag code="MN" alt="Mongolian" width={30} height={20} />
-        </button>
-      )}
+    <div>
+      <button
+        onClick={() => changeLanguage(language === "en" ? "mn" : "en")}
+        className="flex items-center space-x-2 p-2"
+      >
+        <Flag code={language === "en" ? "GB" : "MN"} alt={language === "en" ? "English" : "Mongolian"} width={30} height={20} />
+      </button>
     </div>
   );
 };
